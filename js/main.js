99e71aade4e64474fed818136888ca7bdde0c3b6
Khaled Radwan <khaledradwan96@gmail.com>
2024-07-14 22:37:53 +0300

    edit code structure + start code of function filterData()

--- js/main.js
+++ js/main.js
@@ -2,30 +2,53 @@
 async function getData(){
     const api = 'js/data.json'
     let response = await fetch(api)
-    response = await response.json()
-    // console.log(response) // => for testing
-    displayData(response) // => call displayData function
-}
-
-// ========== function to display data ==========
-function displayData(data) {
+    let data = await response.json()
     let customers = data.customers
     let transactions = data.transactions
-    let cartona = ''
-    for(let i=0; i<customers.length; i++){
-        for(let j=0; j<transactions.length; j++){
-            if(transactions[j].customer_id === customers[i].id){
-                cartona += 
+    // console.log(response) // => for testing
+    init(customers, transactions) // => call displayData function
+    // filterData(response)
+}
+getData() // => to start app
+
+
+function init(customers, transactions) {
+    let container = document.getElementById('data-container')
+    let customerFilter = document.getElementById('customerFilter')
+    let amountFilter = document.getElementById('amountFilter')
+    
+    // ========== function to display data ==========
+    function displayData(customers, transactions){
+        let cartona = ''
+        for(let i=0; i<customers.length; i++){
+            for(let j=0; j<transactions.length; j++){
+                if(transactions[j].customer_id === customers[i].id){
+                    cartona += 
                     `<tr>
                         <td>${transactions[j].id}</td>
                         <td>${customers[i].name}</td>
                         <td>${transactions[j].date}</td>
                         <td>${transactions[j].amount}</td>
                     </tr>`
+                }
             }
         }
+        container.innerHTML = cartona
     }
-    document.getElementById('data-container').innerHTML = cartona
+
+    // ========== function filter data ==========
+    function filterData(){
+        container.innerHTML = ''
+        const customerName = customerFilter.value.toLowerCase();
+        const amount = amountFilter.value;
+        let filteredTransactions = [];
+        for(let i=0; i< transactions.length; i++){
+            console.log(filteredTransactions)
+        }
+        displayData(filteredTransactions)
+    }
+    customerFilter.addEventListener('input', filterData)
+    amountFilter.addEventListener('input', filterData)
+    displayData(customers, transactions)
 }
 
-getData()
