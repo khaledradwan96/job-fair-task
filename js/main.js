2f80fcf96b47e0a955016bb4cf7418620fd288fb
Khaled Radwan <khaledradwan96@gmail.com>
2024-07-14 04:09:20 +0300

    add code of function displayData(data)

--- js/main.js
+++ js/main.js
@@ -9,7 +9,24 @@ async function getData(){
 
 // ========== function to display data ==========
 function displayData(data) {
-
+    let customers = data.customers
+    let transactions = data.transactions
+    let cartona = ''
+    for(let i=0; i<customers.length; i++){
+        for(let j=0; j<transactions.length; j++){
+            if(transactions[j].customer_id === customers[i].id){
+                // console.log(customers[i].name, transactions[j])
+                cartona += 
+                    `<tr>
+                        <td>${i+1}</td>
+                        <td>${customers[i].name}</td>
+                        <td>${transactions[j].date}</td>
+                        <td>${transactions[j].amount}</td>
+                    </tr>`
+            }
+        }
+    }
+    document.getElementById('data-container').innerHTML = cartona
 }
 
 getData()
